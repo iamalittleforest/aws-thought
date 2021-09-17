be5b00cb02fbc089e81f0fe675d22a78fbf2694d
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-09-16 21:34:54 -0700

    create the params object

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -8,4 +8,26 @@ AWS.config.update({
 });
 
 // Create the DynamoDB service object
-const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'}); 
+const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+
+// Create a params object
+const params = {
+  TableName: "Thoughts",
+  KeySchema: [
+    // Partition key
+    { AttributeName: "username", KeyType: "HASH" },
+    // Sort key
+    { AttributeName: "createdAt", KeyType: "RANGE" }
+  ],
+  AttributeDefinitions: [
+    // String data type
+    { AttributeName: "username", AttributeType: "S" },
+    // Number data type
+    { AttributeName: "createdAt", AttributeType: "N" }
+  ],
+  ProvisionedThroughput: {
+    // Maximum capacities
+    ReadCapacityUnits: 10,
+    WriteCapacityUnits: 10
+  }
+};

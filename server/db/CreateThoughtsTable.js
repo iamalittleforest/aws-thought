b4235fc2346525801af4ef98469dd97ee6752a7b
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-09-16 21:27:38 -0700

    create the DynamoDB service object

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -1,8 +1,11 @@
 // Import the aws-sdk package
 const AWS = require('aws-sdk');
 
-// MOdify the AWS config object
+// Modify the AWS config object
 AWS.config.update({
   region: "us-east-2",
   endpoint: "http://localhost:8000"
 });
+
+// Create the DynamoDB service object
+const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'}); 
